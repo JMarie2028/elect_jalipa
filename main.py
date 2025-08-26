9f238c71ebed321b71da3564b3b1c29aa22f0c95
J.Marie2028 <jalipajm2028@wvc.pshs.edu.ph>
2025-08-26 12:47:16 +0800

    change no three

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print("Hello, GitHub!!")
 print("Hello, GitHub!")
+print("Hello, GitHub!")
 git remote add origin https://github.com/JMarie2028/CS_Elective.git
 git branch -M main
 git push -u origin main
