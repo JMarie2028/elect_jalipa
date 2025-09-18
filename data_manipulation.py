038e9c0ef24c6ec11287d4489ab0d3a4aa9af73b
J.Marie2028 <jalipajm2028@wvc.pshs.edu.ph>
2025-09-18 13:58:46 +0800

    Initial commit: Added reflection.txt

--- data_manipulation.py
+++ data_manipulation.py
@@ -16,41 +16,13 @@ print("Average AQI:", average_aqi)
 print("Unique AQI Values (Set):", unique_aqi)
 print("AQI Frequency (Dictionary):", aqi_count)
 
-students = [("Jay", 15, "10th", "Photon"),("Johnica", 15, "9th", "Photon"),
-("Janel", 15, "11th", "Tau"),("Mraye", 15, "10th", "Graviton"),
-("Adi", 15, "12th", "Tau")]
 
-
-third = students[2]
-print("Info of the 3rd Student:")
-print("Name:", third[0])
-print("Grade Level:", third[2])
-print("Section:", third[3])
-print()  
-
-grades = {
-    "Jay": 85,
-    "Johnica": 78,
-    "Janel": 92,
-    "Mraye": 88,
-    "Adi": 81
-}
-
-print("Grade of first student (Jay):", grades["Jay"])
-
-grades["Jay"] = 90
-print("Grade of first student (Jay) [UPDATED]:", grades["Jay"])
-
-print()
-students.append(("Scott", 15, "10th", "Photon"))
-grades["Scott"] = 100
-print("Added new student named Scott with grade:", grades["Scott"])
 git init
 git add .
 git commit -m "Initial commit"
 
 To track and commit the file
-git add helloworld.py
+git add helloworld.pygit commit -m "Initial commit"
 
 git commit -m "Initial commit: Added main.py"
 
